eca1d210d1bf05ebda63528bfe5998d98be840ff
Павел <31769746+KosolapovPavel@users.noreply.github.com>
2019-09-16 21:36:18 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,20 @@
+const n = 23
+let str = ''
+function reverStr(str) {
+    return str.split("").reverse().join("");
+}
+if (n===1){console.log('1');}
+else if (1 < n && n < 1000){
+  console.log('1');
+  for(i=1;i<n;i++){
+    if(i%2){
+    str = str + (i).toString() +'-';
+    console.log(reverStr(str+(i+1)))
+    }
+    else{
+    str = str + (i).toString() +'-';
+    console.log(str+(i+1))
+    }
+  } 
+}
+else {console.log('Vrong number!')}
