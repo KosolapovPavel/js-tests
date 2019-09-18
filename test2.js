96e05ced43a9d8380b570ff2bd953cce6c8709d0
Павел <31769746+KosolapovPavel@users.noreply.github.com>
2019-09-18 20:35:02 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -2,7 +2,7 @@ const n = 23
 let str = ''
 if (n === 1) { console.log('1'); } else if (1 < n && n < 1000) {
   console.log('1');
-  for (i = 2; i < n; i++) {
+  for (i = 2; i <= n; i++) {
 
     if (i % 2) {
 
